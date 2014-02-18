7a142077e1a1d061271c2740d7b8e7c3c9947e1f
Jalvani <joseph.finlayson@gmail.com>
2014-02-18 17:20:53 +0000

    Update XFCE-Arch-RPi.sh

--- XFCE-Arch-RPi.sh
+++ XFCE-Arch-RPi.sh
@@ -6,17 +6,17 @@ pacman -S --noconfirm sudo adduser bash-completion
 # adduser nonroot
 echo "joseph  ALL=(ALL) ALL" >> /etc/sudo
 pacman -Syu --noconfirm
-#pacman -S --noconfirm chromium-browser
-#pacman -S --noconfirm xorg-xinit xorg-server xorg-server-utils xterm
-#pacman -S --noconfirm xfce4
-#pacman -S --noconfirm mesa xf86-video-fbdev xf86-video-vesa
-#pacman -S --noconfirm slim
-#systemctl enable slim.service
-#systemctl enable graphical.target
+pacman -S --noconfirm chromium-browser
+pacman -S --noconfirm xorg-xinit xorg-server xorg-server-utils xterm
+pacman -S --noconfirm xfce4
+pacman -S --noconfirm mesa xf86-video-fbdev xf86-video-vesa
+pacman -S --noconfirm slim
+systemctl enable slim.service
+systemctl enable graphical.target
 cp ~/.xinitrc /home/nonroot/
 chmod 777 /home/nonroot/.xinitrc
 
-#curl https://raw.github.com/jalvani/arch-desktop-environments/master/xinitrc >> ~/.xinitrc
-#curl https://raw.github.com/jalvani/arch-desktop-environments/master/bash_profile >> ~/.bash_profile
+curl https://raw.github.com/jalvani/arch-desktop-environments/master/xinitrc >> ~/.xinitrc
+curl https://raw.github.com/jalvani/arch-desktop-environments/master/bash_profile >> ~/.bash_profile
 
 reboot
