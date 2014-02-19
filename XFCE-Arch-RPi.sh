d244aa72d6f093df4d96ba6eb878aa8e7452fb55
Jalvani <joseph.finlayson@gmail.com>
2014-02-19 23:44:22 +0000

    Update XFCE-Arch-RPi.sh

--- XFCE-Arch-RPi.sh
+++ XFCE-Arch-RPi.sh
@@ -2,17 +2,14 @@ echo "Run this script as root only."
 echo "You can hit Ctrl+c in next 10 seconds if you don't want to continue."
 sleep 10
 # adduser nonroot
-mv .confid ~/.config
+mv .config ~/.config
 mv sources.list /etc/apt/sources.list
 
 apt-get update
-apt-get -y  chromium-browser
+apt-get -y  chromium-browser redshift 
 apt-get -y  apache2 php5 android-tools-adb	caffeine cups firefox nautilus mysql-server-core-5.5
 apt-get -y  mysql-client-5.5 nodejs-dev nodejs	php5-sqlite rails sublime-text
+wget -qO- https://toolbelt.heroku.com/install-ubuntu.sh | sh
 
-chmod 777 /home/nonroot/.xinitrc
-
-curl https://raw.github.com/jalvani/arch-desktop-environments/master/xinitrc >> ~/.xinitrc
-curl https://raw.github.com/jalvani/arch-desktop-environments/master/bash_profile >> ~/.bash_profile
 
 reboot
